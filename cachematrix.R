d60ba680b651fc8bcef38c6bfe98cfbea7d3725d
Sonal Goswami <68932077+sonalgoswami210@users.noreply.github.com>
2020-08-23 07:06:22 +0530

    Update cachematrix.R
    my submission for week 3 R programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## I set the input as x which is a matrix 
+## set s as Null
+## changed every reference of mean to inverse
+## all going well, this should get me the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+         s <- NULL
+  set <- function(y){
+  x <<- y
+  s <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) s <<- inverse
+  getInverse <- function() s 
+  list(set = set, get = get, 
+  setInverse = setInverse, 
+  getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## same here changed mean to solve and changed message and m to t
 cacheSolve <- function(x, ...) {
+        t <- x$getsolve()
+  if(!is.null(t)) {
+    message("getting inversed matrix")
+    return(t)
+  }
+  data <- x$get()
+  t <- solve(data, ...)
+  x$setsolve(t)
+  t
         ## Return a matrix that is the inverse of 'x'
 }
